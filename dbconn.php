db27277744fd4e086f3d5161cd1ee92e3645a245
zyangee <zyangee2@gmail.com>
2024-11-04 14:31:20 +0900

    Add GitHub Actions workflow

--- dbconn.php
+++ dbconn.php
@@ -3,24 +3,10 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-$current_file = basename($_SERVER['PHP_SELF']);
-
-// a페이지나 b페이지가 아닐 경우 로그인 페이지로 다시 이동
-// if ($current_file != 'login.php' && $current_file != 'register.php' && $current_file != 'main.php') {
-     //echo "<script>alert('로그인이 필요합니다');</script>";
-
-     //echo "<script>location.href='login.php';</script>"; //로그인 후 이동할 페이지
-     //exit;
-// }
-?>
-
-<?php
-$serverName = "mysql";
-//$serverName = "210.217.27.205";
-$database = "bank";
-
-$uid = "bankuser1";
-$pwd = "Bankuser1!";
+$serverName = getenv('DB_SERVER');
+$database = getenv('DB_NAME');
+$uid = getenv('DB_USERNAME');
+$pwd = getenv('DB_PASSWORD');
 
 try {
     $conn = new PDO("mysql:host=$serverName;dbname=$database", $uid, $pwd);
